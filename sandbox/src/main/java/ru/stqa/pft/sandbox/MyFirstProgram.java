dff20cdbc07aba19844d7c91a8d5d524cc8dddb6
Kirill <37121507+d2side@users.noreply.github.com>
2020-04-05 22:45:46 +0200

    Math functions added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,23 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello world");
+    hello("Kirill");
+    hello("Alex");
+
+    double l = eq(7);
+
+    System.out.println(l);
+    System.out.println(eq(9, 7));
+  }
+
+  static void hello(String name) {
+    System.out.println("Hello, " + name);
+  }
+
+  public static double eq(double l) {
+    return l * l;
+  }
+  public static double eq(double l, double b) {
+    return l * b;
   }
 }
-    
