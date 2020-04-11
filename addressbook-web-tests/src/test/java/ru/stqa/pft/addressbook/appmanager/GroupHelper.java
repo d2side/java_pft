32eee3d1e02be7eb0e03c39deaaf2151e7639359
Kirill <37121507+d2side@users.noreply.github.com>
2020-04-11 23:26:48 +0200

    Revert "Revert "raw changes of List<GroupData>""
    This reverts commit 82b9d4e49453e1d604172ef3c88e0da900c10e42.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,7 +2,11 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupHelper extends HelperBase {
 
@@ -62,5 +66,12 @@ public class GroupHelper extends HelperBase {
 
   public int getGroupCount() {
     return driver.findElements(By.name("selected[]")).size();
+  }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = driver.findElements(By.cssSelector("span.group"));
+
+    return groups;
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,19 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactCreationTest extends TestBase {
 
   @Test
-  public void testContactCreation() throws Exception {
-    int before = app.getContactHelper().countContacts();
+  public void testContactCreation() {
+    int before = app.getContactHelper().getContactsCount();
     app.getContactHelper().returnToHomePage();
     app.getContactHelper().createContact(new ContactData("Nadia6", "Hz", "group3"));
     app.getContactHelper().returnToHomePage();
-    int after = app.getContactHelper().countContacts();
+    int after = app.getContactHelper().getContactsCount();
     Assert.assertEquals(after, before + 1);
   }
 
