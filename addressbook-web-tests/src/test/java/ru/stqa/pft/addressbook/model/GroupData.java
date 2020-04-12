0207279638dff67433e78ea60fff4c9ac2b48813
Kirill <37121507+d2side@users.noreply.github.com>
2020-04-12 04:20:39 +0200

    group's unic ID's added but needs to configure the Constructor

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,14 +3,28 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+  private final String id;
   private final String name;
   private final String header;
   private final String footer;
 
+
   public GroupData(String name, String header, String footer) {
+    this.id = null;
     this.name = name;
     this.header = header;
     this.footer = footer;
+  }
+
+  public GroupData(String id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public String getId() {
+    return id;
   }
 
   public String getName() {
@@ -28,7 +42,8 @@ public class GroupData {
   @Override
   public String toString() {
     return "GroupData{" +
-            "name='" + name + '\'' +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
             '}';
   }
 
